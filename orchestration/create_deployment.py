23a27ff301bc5baf3c755d9fafb3b2de44b236c2
Josema <josemamup@gmail.com>
2024-07-25 16:34:25 +0200

    Add test flow

--- orchestration/create_deployment.py
+++ orchestration/create_deployment.py
@@ -2,9 +2,10 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/prefecthq/demos.git", 
-        entrypoint="repo_info.py:get_repo_info"
+        source="https://github.com/josmunpen/laliga-oracle.git", 
+        entrypoint="orchestration/code_flow.py:hola_mundo",
+        ref="prefect-aux"
     ).deploy(
-        name="deploy_code_demos", 
+        name="deploy_code_test_liga", 
         work_pool_name="work-pool-2", 
     )
